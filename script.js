db1499ca43ae5a6f2584cd557b53af4b00b2ff60
9ziggy9 <davidarogers@protonmail.com>
2021-09-19 04:34:07 -0700

    prepare to splode

--- script.js
+++ script.js
@@ -8,10 +8,11 @@ const colorArray = ['black', 'white', 'blue',
     'purple', 'orange', 'teal'
 ];
 let colorNum = 0;
+let MOUSE_DOWN = false;
 
 const mouse = {
-    x: null,
-    y: null,
+    x: undefined,
+    y: undefined,
     radius: 150
 };
 
@@ -34,26 +35,45 @@ window.addEventListener('click', event => {
     // colorNum++;
 });
 
-// DRAWING VELOCITY VECTOR /////////////////////////////////////////////////////
+// DRAWING VELOCITY VECTOR ///////////////////////////////////////////////////
 window.addEventListener('mousedown', event => {
-    console.log('holding down');
-    velVector.x_0 = event.x;
-    velVector.y_0 = event.y;
+    if (event.button === 0) {
+        console.log('holding down');
+        MOUSE_DOWN = true;
+        velVector.x_0 = event.x;
+        velVector.y_0 = event.y;
+    }
 });
-window.addEventListener('mouseup', event => {
-    console.log('let go');
-    colorNum++;
-    const radius = 10;
+window.addEventListener('mousemove', event => {
     velVector.x_1 = event.x;
     velVector.y_1 = event.y;
-    velVector.mag = Math.sqrt(Math.pow(velVector.x_1 - velVector.x_0, 2) +
-                              Math.pow(velVector.y_1 - velVector.y_0, 2));
-    particleArray.push(new Particle(velVector.x_0, velVector.y_0, radius,
-                                    colorArray[colorNum % colorArray.length],
-                                    4.4, 0.4,
-                                    (velVector.x_1 - velVector.x_0) / 10,
-                                    (velVector.y_1 - velVector.y_0) / 10));
-    console.log(velVector.mag);
+});
+window.addEventListener('mouseup', event => {
+    if (event.button === 0) {
+        console.log('let go');
+        MOUSE_DOWN = false;
+        colorNum++;
+        const radius = 10;
+        velVector.x_1 = event.x;
+        velVector.y_1 = event.y;
+        velVector.mag = Math.sqrt(Math.pow(velVector.x_1 - velVector.x_0, 2) +
+                                  Math.pow(velVector.y_1 - velVector.y_0, 2));
+        particleArray.push(new Particle(velVector.x_0, velVector.y_0, radius,
+                                        colorArray[colorNum % colorArray.length],
+                                        4.4, 0.4,
+                                        (velVector.x_1 - velVector.x_0) / 5,
+                                        (velVector.y_1 - velVector.y_0) / 5));
+        console.log(velVector.mag);
+        for (let key in velVector)
+            velVector[key] = null;
+        console.log(velVector);
+    }
+});
+///////////////////////////////////////////////////////////////////////////////
+// BLOW SHIT UP /////////////////////////////////////////////////////////////
+window.addEventListener('contextmenu', event => {
+    event.preventDefault();
+    console.log('BABOOM');
 });
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -115,18 +135,33 @@ class Particle {
 }
 
 function animate() {
+    // CLEARING SCREEN
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // DRAWING PARTICLES
     for (let i = 0; i < particleArray.length; i++) {
         particleArray[i].inelasticFall();
         particleArray[i].fire();
         particleArray[i].draw();
         // Clean up
-        if (particleArray[i].x > canvas.width ||
-            particleArray[i].x < 0) {
+        if (particleArray[i].x > canvas.width + particleArray[i].radius ||
+            particleArray[i].x < -particleArray[i].radius) {
             particleArray.splice(i, 1);
             console.log(`particle ${i} removed`);
+            console.log(particleArray);
         }
     }
+
+    // AIMING ARROW
+    if (MOUSE_DOWN) {
+        ctx.beginPath();
+        ctx.strokeStyle = 'gray';
+        ctx.moveTo(velVector.x_0, velVector.y_0);
+        ctx.lineTo(velVector.x_1, velVector.y_1);
+        ctx.stroke();
+    }
+   // NEXT FRAME
     requestAnimationFrame(animate);
 }
+
 animate();
